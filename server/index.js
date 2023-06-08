71d8389bf924ecb10f465e464c94e0e8269e92a1
ajakhar0 <85440335+ajakhar0@users.noreply.github.com>
2023-06-08 22:57:20 +0530

    init commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from "express";
+import bodyParser from "body-parser";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./mongodb/connect.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(bodyParser.json());
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", async (req, res) => {
+  res.send("hello from DALL-E!");
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(8080, () =>
+      console.log("Server has started on port http://localhost:8080")
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
